88b3b90669411912931433c1572fb03a07723f2e
Kaminyou <ikaminyou@gmail.com>
2020-12-28 23:23:13 +0800

    <fix> model dynamic

--- utils/model.py
+++ utils/model.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 import torch
 
 class HemoResNet18(nn.Module):
-    def __init__(self, n_classes):
+    def __init__(self, in_channels = 3, n_classes=5):
         super().__init__()
         resnet = models.resnet18(pretrained=True)
+        if in_channels != 3:
+            resnet.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3,bias=False)
         resnet.fc = nn.Sequential(
             nn.Dropout(p=0.2),
             nn.Linear(in_features=resnet.fc.in_features, out_features=n_classes)
@@ -15,9 +17,11 @@ class HemoResNet18(nn.Module):
         return self.base_model(x)
 
 class HemoResNet50(nn.Module):
-    def __init__(self, n_classes):
+    def __init__(self, in_channels = 3, n_classes=5):
         super().__init__()
         resnet = models.resnet50(pretrained=True)
+        if in_channels != 3:
+            resnet.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3,bias=False)
         resnet.fc = nn.Sequential(
             nn.Dropout(p=0.2),
             nn.Linear(in_features=resnet.fc.in_features, out_features=n_classes)
@@ -27,9 +31,11 @@ class HemoResNet50(nn.Module):
         return self.base_model(x)
 
 class HemoResnext50(nn.Module):
-    def __init__(self, n_classes):
+    def __init__(self, in_channels = 3, n_classes=5):
         super().__init__()
         resnet = models.resnext50_32x4d(pretrained=True)
+        if in_channels != 3:
+            resnet.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3,bias=False)
         resnet.fc = nn.Sequential(
             nn.Dropout(p=0.2),
             nn.Linear(in_features=resnet.fc.in_features, out_features=n_classes)
