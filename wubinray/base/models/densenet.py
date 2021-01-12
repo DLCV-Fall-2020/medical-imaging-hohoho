0b77fabb2c338a0bf747629558f49a9fd3a0d401
wubinary <wubinray@gmail.com>
2021-01-12 13:49:22 +0800

    densnet121 featrue extract

--- wubinray/base/models/densenet.py
+++ wubinray/base/models/densenet.py
@@ -12,8 +12,14 @@ class HemoDenseNet121(nn.Module):
         densenet.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
             nn.ReLU(True),
-            nn.Linear(in_features=densenet.fc.in_features, out_features=n_classes)
+            nn.Linear(in_features=densenet.classifier.in_features, out_features=n_classes)
         )
         self.base_model = densenet
     def forward(self, x):
         return self.base_model(x)
+
+    def featrue(self, x):
+        for layer in list(self.base_model.children())[:-1]:
+            x = layer(x)
+        return x
+
